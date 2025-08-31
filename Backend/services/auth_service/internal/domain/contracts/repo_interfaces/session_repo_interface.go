b187f6e4c6a175e33a50a8a04e4751df9330f2d8
Kalki767 <kalkidankidane3@gmail.com>
2025-08-31 12:55:05 +0300

    Modify session repository interface

--- Backend/services/auth_service/internal/domain/contracts/repo_interfaces/session_repo_interface.go
+++ Backend/services/auth_service/internal/domain/contracts/repo_interfaces/session_repo_interface.go
@@ -9,8 +9,9 @@ import (
 type SessionRepoInterface interface {
 	AddSession(session *entity.Session) (*entity.Session, error)
 	GetById(Id uuid.UUID) (*entity.Session, error)
+	GetAll(userId uuid.UUID)([]*entity.Session, error)
 	RevokeSession(Id uuid.UUID) error
 	RevokeForAllUser(userId uuid.UUID) error
-	RevokeAllExceptCurrent(userId uuid.UUID, keepsessionId uuid.UUID) (bool, error)
+	RevokeAllExceptCurrent(userId uuid.UUID, keepsessionId uuid.UUID) error
 	UpdateLastUsed(Id uuid.UUID) error
 }
