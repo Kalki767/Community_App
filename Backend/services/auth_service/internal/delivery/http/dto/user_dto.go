f98aa12c08b3e959da94dc1cc7e425b837a6c7b8
Kalki767 <kalkidankidane3@gmail.com>
2025-09-06 08:12:30 +0300

    modify user dto to include more dto for accepting user inputs and outputs

--- Backend/services/auth_service/internal/delivery/http/dto/user_dto.go
+++ Backend/services/auth_service/internal/delivery/http/dto/user_dto.go
@@ -3,25 +3,25 @@ package dto
 import "github.com/google/uuid"
 
 type UserDto struct {
-	ID          uuid.UUID
-	FullName    string
-	Email       string
-	Username    string
-	PhoneNumber string
-	Country     string
-	IsVerified  bool
+	ID          uuid.UUID `json:"id"`
+	FullName    string    `json:"full_name"`
+	Email       string    `json:"email"`
+	Username    string    `json:"username"`
+	PhoneNumber string    `json:"phone_number"`
+	Country     string    `json:"country"`
+	IsVerified  bool      `json:"is_verified"`
 }
 
 type RegisterUser struct {
-	FullName    string
-	Email       string
-	Username    string
-	Password string
-	PhoneNumber string
-	Country     string
+	FullName    string `json:"full_name" binding:"required"`
+	Email       string `json:"email" binding:"required,email"`
+	Username    string `json:"username" binding:"required"`
+	Password    string `json:"password" binding:"required"`
+	PhoneNumber string `json:"phone_number"`
+	Country     string `json:"country"`
 }
 
 type LoginRequest struct {
-	Identification string
-	Password string
+	Identification string `json:"identification" binding:"required"`
+	Password       string `json:"password" binding:"required"`
 }
