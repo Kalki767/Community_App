d98cd2c4acfc4e1b28c165cc0bad96c1e4bb2573
Kalki767 <kalkidankidane3@gmail.com>
2025-09-06 08:12:28 +0300

    modify session dto to include more dto for accepting user inputs and outputs

--- Backend/services/auth_service/internal/delivery/http/dto/session_dto.go
+++ Backend/services/auth_service/internal/delivery/http/dto/session_dto.go
@@ -6,12 +6,23 @@ import (
 	"github.com/google/uuid"
 )
 
-type SessionDTO struct {
-	ID uuid.UUID
-	UserID uuid.UUID
-	TokenHash  string     `gorm:"not null"`
-    ExpiresAt  time.Time  `gorm:"index;not null"`
-    UserAgent  string
-    IP         string
-    LastUsedAt time.Time
+type SessionResponseDTO struct {
+	ID uuid.UUID `json:"id"`
+	UserID uuid.UUID `json:"user_id"`
+    UserAgent  string `json:"user_agent"`
+    IP         string `json:"ip"`
+    LastUsedAt time.Time `json:"last_used_at"`
+}
+
+type RefreshRequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+type RefreshResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
+type MessageResponse struct {
+    Message string `json:"message"`
+    Error   string `json:"error,omitempty"`
 }
