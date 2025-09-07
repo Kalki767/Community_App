e18d0864605398ee09db09d311c98311ce09708c
Kalki767 <kalkidankidane3@gmail.com>
2025-09-07 09:15:12 +0300

    modify session usecase to include implementation of is active session function

--- Backend/services/auth_service/internal/domain/contracts/usecase_interfaces/session_usecase_interface.go
+++ Backend/services/auth_service/internal/domain/contracts/usecase_interfaces/session_usecase_interface.go
@@ -7,9 +7,10 @@ import (
 )
 
 type SessionUsecaseInterface interface {
-    ListActiveSessions(userID uuid.UUID) ([]*dto.SessionDTO, error)
-    GetSession(sessionID uuid.UUID) (*dto.SessionDTO, error)
-    Logout(sessionID uuid.UUID) error
-    LogoutAllExcept(userID uuid.UUID, keepSessionID uuid.UUID) error
-    Refresh(refreshToken string) (string, error)
+	ListActiveSessions(userID uuid.UUID) ([]*dto.SessionResponseDTO, error)
+	GetSession(sessionID uuid.UUID) (*dto.SessionResponseDTO, error)
+	Logout(sessionID uuid.UUID) error
+	LogoutAllExcept(userID uuid.UUID, keepSessionID uuid.UUID) error
+	Refresh(refreshToken string) (string, error)
+    IsSessionActive(sessionID uuid.UUID) (bool, error)
 }
